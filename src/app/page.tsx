aa46b6320eab0ab66055b351fad70af9ddf6c7da
Mehebul02 <mehebul2123@gmail.com>
2025-07-28 22:52:47 +0600

    Solved the error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
 import { fetchProductData } from "@/lib/api";
 import CoursePage from "@/pages/Courses";
 
-export default async function Home({
-  searchParams = {},
-}: {
-  params?: never;
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
-  try {
-    const lang = Array.isArray(searchParams.lang) 
-      ? searchParams.lang[0] 
-      : searchParams.lang || "en";
-    
-    const data = await fetchProductData(lang);
-    return <CoursePage data={data} />;
-    
-  } catch (error) {
-    console.error("Failed to load course data:", error);
-    return <div>Error loading course content</div>;
-  }
+
+
+export default async function IELTSCoursePage() {
+
+  const data = await fetchProductData();
+
+  return <CoursePage data={data} />;
 }
